54c542697bc6b99526a74af9a9885595db01ce4e
zcx <zcx980605@sohu.com>
2021-06-04 00:50:40 +0800

    Build for 20210604_更新CSS

---
+++ gitbook/gitbook/gitbook-plugin-baidu-tongji-with-multiple-channel/plugin.js
@@ -0,0 +1,36 @@
+require(["gitbook"], function (gitbook) {
+    gitbook.events.bind("start", function (e, config) {
+        // 插件配置
+        var config = config["baidu-tongji-with-multiple-channel"] || {};
+        var token = config.token;
+        var url = config.url;
+        var multipleChannelConfig = config.multipleChannelConfig;
+
+        // 开启多渠道配置开关并且存在多渠道配置
+        if (JSON.stringify(multipleChannelConfig) !== "{}") {
+            var currentHost = location.hostname;
+            for (var channelHost in multipleChannelConfig) {
+                if (currentHost == channelHost) {
+                    token = multipleChannelConfig[channelHost].token || token;
+                    url = multipleChannelConfig[channelHost].url || url;
+                    break;
+                }
+            }
+        }
+
+        // 自动插入百度统计代码
+        window._hmt = window._hmt || [];
+        if (url !== "" && token !== "") {
+            var hm = document.createElement("script");
+            hm.src = url + '?' + token;
+            var s = document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(hm, s);
+        }
+    });
+
+    // 跟踪受访页面
+    gitbook.events.bind("page.change", function () {
+        var path = window.location.pathname + window.location.search
+        _hmt.push(['_trackPageview', path]);
+    });
+});
